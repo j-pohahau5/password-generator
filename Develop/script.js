3951e048ae6e84e54d5a28aa745628494c4cb84c
Jonathan Pohahau <jonathanpohahau@Jonathans-MBP.attlocal.net>
2022-12-21 22:34:31 -0800

    used function generatePassword to define generate password

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("Generated button has been clicked!")
+
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
