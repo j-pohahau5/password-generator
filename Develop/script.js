d63ae1155ec6b6a21d1f04afdd7c1631f850cd6f
Jonathan Pohahau <jonathanpohahau@Jonathans-MBP.attlocal.net>
2022-12-22 17:56:16 -0800

    was able to add alert to prompt for length, add multiple variables connecting to confirms, give conditional statements to variables connected to confirms, amd lastly made chosenCharacters list away with Array.from

--- Develop/script.js
+++ Develop/script.js
@@ -22,13 +22,56 @@ var generateBtn = document.querySelector("#generate");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var number = "0123456789";
-var chosenCharacter =""
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var chosenCharacter = []
 
 // created "function generatePassword" to define generatePassword and used return to give it value.
+// any time you created a variable because you plan on using it
 function generatePassword() {
   console.log("Generated button has been clicked!")
+  var result = ""
+  // add a prompt for a user to be able to input the amount of characters they need 
+  var length = prompt ("How many characters would you like to have for your password? (Choose between 8 to 128)")
+  // add if to alert the user to enter the correct number
+  if (isNaN(length) || length < 8 || length > 128){
+    alert("You must enter a number between 8 to 128 characters.")
+    generatePassword()
+  }
+  var hasUpperCase = confirm("Would you like upper case letters?")
+  console.log(hasUpperCase)
+  var hasLowerCase = confirm("Would you like lower case letters?")
+  console.log(hasLowerCase)
+  var hasNumbers = confirm("Would you like numbers?")           
+  console.log(hasNumbers)
+  var hasSpecialCharacters = confirm("Would you  like special characters?")
+  console.log(hasSpecialCharacters)
+  if (hasUpperCase === true) {
+    chosenCharacter += upperCase
+    console.log(chosenCharacter)
+  } 
+  if (hasLowerCase === true) {
+    chosenCharacter += lowerCase
+    console.log(chosenCharacter)
+  } 
+  if (hasNumbers === true) {
+    chosenCharacter += number
+    console.log(chosenCharacter)
+  } 
+  if (hasSpecialCharacters === true) {
+    chosenCharacter += specialCharacters
+    console.log(chosenCharacter)
+  } 
+  chosenCharacter = Array.from(chosenCharacter)
+  console.log(chosenCharacter)
+
+  for(var i = 0; i < length; i++) {
+    // choose possible character at random and add it too password until you reach the desire password length
+    length += chosenCharacter; Math.floor(Math.random())
+    console.log(length)
+
+  }
  
-  return "password is generated"
+  return "you clicked the button";
 }
 
 // Write password to the #password input
